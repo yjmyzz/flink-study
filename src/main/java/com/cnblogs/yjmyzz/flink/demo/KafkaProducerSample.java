b3b61d95453cb98ee8e89023e964cddbd4e1338d
junmingyang <junmingyang@trip.com>
2020-11-23 11:06:40 +0800

    add timeWindow示例

--- src/main/java/com/cnblogs/yjmyzz/flink/demo/KafkaProducerSample.java
+++ src/main/java/com/cnblogs/yjmyzz/flink/demo/KafkaProducerSample.java
@@ -1,19 +1,24 @@
 package com.cnblogs.yjmyzz.flink.demo;
 
-import java.util.Properties;
-import java.util.Random;
-
+import com.google.gson.Gson;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @author 菩提树下的杨过
  */
 public class KafkaProducerSample {
 
-    public static String topic = "test1";
+    private static String topic = "test3";
+
+    private static Gson gson = new Gson();
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
     public static void main(String[] args) throws InterruptedException {
         Properties p = new Properties();
@@ -22,13 +27,20 @@ public class KafkaProducerSample {
         p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(p);
 
+        String[] words = new String[]{"hello", "world", "java", "flink", "scala", "spring", "hive", "spark"};
+        Random rnd = new Random();
         try {
             while (true) {
-                String msg = "Hello," + new Random().nextInt(100);
+                Map<String, String> map = new HashMap<>();
+                map.put("word", words[rnd.nextInt(words.length)]);
+                long timestamp = System.currentTimeMillis();
+                map.put("event_timestamp", timestamp + "");
+                map.put("event_datetime", sdf.format(new Date(timestamp)));
+                String msg = gson.toJson(map);
                 ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, msg);
                 kafkaProducer.send(record);
-                System.out.println("消息发送成功:" + msg);
-                Thread.sleep(500);
+                System.out.println("message send success:" + msg);
+                Thread.sleep(10000);
             }
         } finally {
             kafkaProducer.close();
---
+++ src/main/java/com/cnblogs/yjmyzz/flink/demo/KafkaStreamTumblingWindowCount.java
@@ -0,0 +1,106 @@
+package com.cnblogs.yjmyzz.flink.demo;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.flink.api.common.eventtime.*;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.serialization.SerializationSchema;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
+import org.apache.flink.util.Collector;
+
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @author 菩提树下的杨过(http : / / yjmyzz.cnblogs.com /)
+ */
+public class KafkaStreamTumblingWindowCount {
+
+    private final static Gson gson = new Gson();
+    private final static String SOURCE_TOPIC = "test3";
+    private final static String SINK_TOPIC = "test4";
+
+    public static void main(String[] args) throws Exception {
+
+        // 1 设置环境
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //指定使用eventTime作为时间标准
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        // 2. 定义数据
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("zookeeper.connect", "localhost:2181");
+        props.put("group.id", "test-read-group");
+        props.put("deserializer.encoding", "GB2312");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.offset.reset", "latest");
+
+        DataStreamSource<String> text = env.addSource(new FlinkKafkaConsumer011<>(
+                SOURCE_TOPIC,
+                new SimpleStringSchema(),
+                props));
+
+        // 3. 处理逻辑
+        DataStream<Tuple2<String, Integer>> counts = text.assignTimestampsAndWatermarks(new WatermarkStrategy<String>() {
+            @Override
+            public WatermarkGenerator<String> createWatermarkGenerator(WatermarkGeneratorSupplier.Context context) {
+                return new WatermarkGenerator<String>() {
+                    private long maxTimestamp;
+                    private long delay = 1000;
+
+                    @Override
+                    public void onEvent(String s, long l, WatermarkOutput watermarkOutput) {
+                        Map<String, String> map = gson.fromJson(s, new TypeToken<Map<String, String>>() {
+                        }.getType());
+                        String timestamp = map.getOrDefault("event_timestamp", l + "");
+                        maxTimestamp = Math.max(maxTimestamp, Long.parseLong(timestamp));
+                    }
+
+                    @Override
+                    public void onPeriodicEmit(WatermarkOutput watermarkOutput) {
+                        watermarkOutput.emitWatermark(new Watermark(maxTimestamp - delay));
+                    }
+                };
+            }
+        }).flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                //解析message中的json
+                Map<String, String> map = gson.fromJson(value, new TypeToken<Map<String, String>>() {
+                }.getType());
+
+                //收集(类似:map-reduce思路)
+                String word = map.getOrDefault("word", "");
+                if (word != null && word.trim().length() > 0) {
+                    out.collect(new Tuple2<>(word.trim(), 1));
+                }
+
+            }
+        })
+                //按Tuple2里的第0项，即：word分组
+                .keyBy(value -> value.f0)
+                .timeWindow(Time.minutes(1))
+                //然后对Tuple2里的第1项求合
+                .sum(1);
+
+        // 4. 打印结果
+        counts.addSink(new FlinkKafkaProducer010<>("localhost:9092", SINK_TOPIC,
+                (SerializationSchema<Tuple2<String, Integer>>) element -> ("(" + element.f0 + "," + element.f1 + ")").getBytes()));
+        counts.print();
+
+        // execute program
+        env.execute("Kafka Streaming WordCount");
+
+    }
+}
